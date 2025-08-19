a27d376cd2e9cee68607e37c45f1a388d0691e00
Jonathan Borroel <borroeljohn@gmail.com>
2025-08-18 22:32:22 -0400

    Task 3 Get Book Details by Author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  const matches = keys.filter((key)=>books[key]["author"] === author)
+  if(matches.length>0){
+    let bookDetails = {};
+    matches.forEach((isbn, index)=>bookDetails[isbn]=books[isbn]);
+    return res.send(JSON.stringify(bookDetails, null, 4));
+  }else{
+    return res.status(404).json({message:"Author not found"})
+  }
+  
 });
 
 // Get all books based on title
