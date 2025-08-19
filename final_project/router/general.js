8a4c99c0a2d352bd97a3636b2b97dfa0b0e84efd
Jonathan Borroel <borroeljohn@gmail.com>
2025-08-18 21:46:52 -0400

    Task 2 Get Details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,13 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  let book = books[ISBN];
+  if(book){
+    return res.send(JSON.stringify(book, null, 4));
+  }else{
+    return res.status(404).json({message:"ISBN not found"});
+  }
  });
   
 // Get book details based on author
